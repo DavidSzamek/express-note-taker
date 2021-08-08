6a774023f11907edd78d402e72e6b73bfa1d3205
David Szamek <davidszamek@gmail.com>
2021-08-08 15:08:36 +1000

    added server.js functionality

---
+++ server.js
@@ -0,0 +1,28 @@
+// Dependencies
+const express = require("express");
+const path = require("path");
+
+// Initialise PORT 
+const PORT = process.env.PORT || 8080;
+
+// Create an express server 
+const app = express();
+
+// Initialise data parsing
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.static('public'));
+
+// Created routes that will serve up the correct pages for the user
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Initialise server. If error, console.log the error. 
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log(err);
+        return
+    } 
+    console.log(`Server is listening on port: ${PORT}`)
+})
+
