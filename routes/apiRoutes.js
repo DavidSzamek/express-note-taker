6ce9e860fbc8aa50843025dec8139a1b66db6787
David Szamek <davidszamek@gmail.com>
2021-08-11 13:36:37 +1000

    Updated delete functionality of notes and test db data

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,17 +5,35 @@ const fs = require("fs");
 // Routing
 module.exports = function (app) {
     // GET Request
-    app.get("/api/notes/", function (req, res) {
+    app.get("/api/notes", function (req, res) {
         res.json(db);
     });
 
     // POST Request
     app.post("/api/notes", function (req, res) {
         db.push(req.body);
-    });
-    
-    fs.writeFile("./db/db,json", JSON.stringify(db), function () {
-        res.json(db);
-    })
 
-}
+        db.forEach((obj, i) => {
+            obj.id = i +1;
+        });
+
+        fs.writeFile("./db/db.json", JSON.stringify(db), function () {
+            res.json(db);
+        });
+    });
+
+      // DELETE Request
+      app.delete("/api/notes/:id", function (req, res) {
+        const id = req.params.id;
+
+        db.splice(id - 1, 1);
+
+        db.forEach((obj, i) => {
+            obj.id = i + 1;
+        });
+
+        fs.writeFile("./db/db.json", JSON.stringify(db), function () {
+            res.json(db);
+        });
+    });
+};
